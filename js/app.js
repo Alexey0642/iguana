a1cc90753f3d9d06b6ea3f09a42d7ca40f97b39e
Alexey0642 <100562051+Alexey0642@users.noreply.github.com>
2022-02-28 12:16:55 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+	$('.slider').slick({
+		dots:true
+	});
+});
+
+$(document).ready(function() {
+	$('.header-burger').click(function(event){
+		$('.header-burger,.menu-hamb').toggleClass('trans');
+	});
+});
+
+
+	
